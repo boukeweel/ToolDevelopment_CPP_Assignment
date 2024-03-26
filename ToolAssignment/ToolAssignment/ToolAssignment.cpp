3d91c1016bd552dd8f357b0b6fc3a1a479746dcf
boukeweel <boukeweel@outlook.com>
2024-03-26 15:20:37 +0100

    Project

---
+++ ToolAssignment/ToolAssignment/ToolAssignment.cpp
@@ -0,0 +1,12 @@
+﻿// ToolAssignment.cpp : Defines the entry point for the application.
+//
+
+#include "ToolAssignment.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
